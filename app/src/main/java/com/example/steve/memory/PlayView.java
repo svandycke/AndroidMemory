0f6b00422e6ad6b54b2e0e98ff84e61ade4b9282
svandycke <svandycke@gmail.com>
2016-12-30 10:06:36 +0100

    Ajout et placement des images aléatoirement

--- app/src/main/java/com/example/steve/memory/PlayView.java
+++ app/src/main/java/com/example/steve/memory/PlayView.java
@@ -5,14 +5,27 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import static android.R.attr.bitmap;
+import static android.R.attr.width;
+import static com.example.steve.memory.R.attr.height;
 import static java.lang.System.in;
 
 /**
@@ -24,7 +37,13 @@ public class PlayView extends SurfaceView implements SurfaceHolder.Callback, Run
     private Thread  cv_thread;
     SurfaceHolder holder;
     private boolean in = true;
-    private int TestThread = 0;
+
+
+    private Resources 	mRes;
+    private Context 	mContext;
+
+    ArrayList<Bitmap> cartesDisponnibles = new ArrayList<Bitmap>();
+    ArrayList<Bitmap> cartes = new ArrayList<Bitmap>();
 
     public PlayView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -33,28 +52,35 @@ public class PlayView extends SurfaceView implements SurfaceHolder.Callback, Run
         holder = getHolder();
         holder.addCallback(this);
 
+        // Fond transparent SurfaceView
+        this.setZOrderOnTop(true);
+        this.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+
+        mContext	= context;
+        mRes 		= mContext.getResources();
 
         // creation du thread
         cv_thread   = new Thread(this);
         // prise de focus pour gestion des touches
         setFocusable(true);
 
+        // Initialisation des paramètres
         initparameters();
 
+
     }
-    // callback sur le cycle de vie de la surfaceview
+    // Callback : cycle de vie de la surfaceview
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
         initparameters();
-        Log.i("-> FCT <-", "surfaceChanged "+ width +" - "+ height);
     }
 
     public void surfaceCreated(SurfaceHolder arg0) {
-        Log.i("-> FCT <-", "surfaceCreated");
+
     }
 
 
     public void surfaceDestroyed(SurfaceHolder arg0) {
-        Log.i("-> FCT <-", "surfaceDestroyed");
+
     }
 
     public void pause() {
@@ -65,7 +91,9 @@ public class PlayView extends SurfaceView implements SurfaceHolder.Callback, Run
                 cv_thread.join(); //--tente de relancer le Thread
             }
             catch (InterruptedException e)
-            {e.printStackTrace();}
+            {
+                e.printStackTrace();
+            }
             break;
         }
         cv_thread=null;
@@ -86,14 +114,11 @@ public class PlayView extends SurfaceView implements SurfaceHolder.Callback, Run
     public void run() {
         Canvas c = null;
         while (in) {
-
-            TestThread = (TestThread+1);
-            Log.i("Information",String.valueOf(TestThread));
-
             try {
                 cv_thread.sleep(40);
                 try {
                     c = holder.lockCanvas(null);
+                    nDraw(c);
                 } finally {
                     if (c != null) {
                         holder.unlockCanvasAndPost(c);
@@ -103,6 +128,44 @@ public class PlayView extends SurfaceView implements SurfaceHolder.Callback, Run
                 Log.e("-> RUN <-", "PB DANS RUN");
             }
         }
+    }
+
+    // dessin du jeu (fond uni, en fonction du jeu gagne ou pas dessin du plateau et du joueur des diamants et des fleches)
+    private void nDraw(Canvas canvas) {
+
+        // Détermination de la taille des images et des marges
+        int tailleImage = (canvas.getWidth()/5);
+        int tailleMarge = (tailleImage/3);
+
+        int carte = 0;
+
+        for(int j=0; j<5; j++){
+            for(int k=0; k<4; k++){
+                Bitmap image = BITMAP_RESIZER(cartes.get(carte),(tailleImage),(tailleImage));
+                canvas.drawBitmap(image, ((tailleImage + tailleMarge)*k), ((tailleImage + tailleMarge)*j), new Paint());
+                carte++;
+            }
+        }
+    }
+
+    // Fonction qui permet de redimensionner un bitmap
+    public Bitmap BITMAP_RESIZER(Bitmap bitmap,int newWidth,int newHeight) {
+        Bitmap scaledBitmap = Bitmap.createBitmap(newWidth, newHeight, Bitmap.Config.ARGB_8888);
+
+        float ratioX = newWidth / (float) bitmap.getWidth();
+        float ratioY = newHeight / (float) bitmap.getHeight();
+        float middleX = newWidth / 2.0f;
+        float middleY = newHeight / 2.0f;
+
+        Matrix scaleMatrix = new Matrix();
+        scaleMatrix.setScale(ratioX, ratioY, middleX, middleY);
+
+        Canvas canvas = new Canvas(scaledBitmap);
+        canvas.setMatrix(scaleMatrix);
+        canvas.drawBitmap(bitmap, middleX - bitmap.getWidth() / 2, middleY - bitmap.getHeight() / 2, new Paint(Paint.FILTER_BITMAP_FLAG));
+
+        return scaledBitmap;
+
     }
 
     // fonction permettant de recuperer les evenements tactiles
@@ -125,11 +188,38 @@ public class PlayView extends SurfaceView implements SurfaceHolder.Callback, Run
     }
 
     public void initparameters() {
+
+        repartitionCartes();
+
         if ((cv_thread!=null) && (!cv_thread.isAlive())) {
             cv_thread.start();
             Log.e("-FCT-", "cv_thread.start()");
         }
     }
 
+    // Fonction qui réparti les carte
+    private void repartitionCartes(){
+        int numero;
+        for(int i=1; i<=10; i++){
+
+            if(i>10){
+                numero = (i-10);
+            }else{
+                numero = i;
+            }
+
+            int resID = getResources().getIdentifier("animaux" + numero , "drawable", mContext.getPackageName());
+            cartesDisponnibles.add(BitmapFactory.decodeResource(mRes, resID));
+        }
+
+        for(int z=0; z<10; z++){
+            int nbCarteDispo = cartesDisponnibles.size();
+            Random r = new Random();
+            int aleatoire = r.nextInt(nbCarteDispo);
+
+            cartes.add(cartesDisponnibles.get(aleatoire));
+            cartesDisponnibles.remove(aleatoire);
+        }
+    }
 
 }
